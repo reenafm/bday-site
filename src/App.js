2e509a6bcec76ed30f476a6e834af80adc07a7ac
reenafm <reenafied@gmail.com>
2025-08-17 22:59:58 -0700

    fith commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import audio1 from './myOrdinaryLife.mp3';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Homepage from './components/homepage/Homepage';
 import Pagetwo from './components/pagetwo/Pagetwo';
-import Navbar  from './components/Navbar';
+
 
 function App() {
   return (
@@ -14,9 +14,9 @@ function App() {
         <Route path="/bday-site" element={<Homepage />} />
         <Route path="/bday-site/pagetwo" element={<Pagetwo />} />
       </Routes>
-    <Navbar></Navbar>
     </BrowserRouter>
   );
 }
 
+// git add . ; git commit -m "fith commit" ; git push ; npm run predeploy ; npm run deploy
 export default App;
