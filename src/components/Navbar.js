71620cf7d2bb69e4ab57e3cf088ffaf77d36cc9d
reenafm <reenafied@gmail.com>
2025-08-17 23:26:24 -0700

    fith commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,9 +6,9 @@ function Navbar() {
   return (
     <nav style= {{ width: '0px' , height: '0px' }}>
       <Link to="/bday-site">
-      <img src="https://i.imgur.com/N9J230M.jpeg"  ></img>
+      <img src="https://i.imgur.com/N9J230M.jpeg" style= {{ width: '0px' , height: '0px' }}></img>
       </Link>
-      <Link to="/bday-site/pagetwo"><img src="https://i.imgur.com/N9J230M.jpeg"  ></img></Link>
+      <Link to="/bday-site/pagetwo"><img src="https://i.imgur.com/N9J230M.jpeg" style= {{ width: '0px' , height: '0px' }} ></img></Link>
     </nav>
   );
 }
