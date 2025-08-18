c76a1acc482efa09da59b2c10b2063302f5f71ee
reenafm <reenafied@gmail.com>
2025-08-17 22:48:43 -0700

    fith commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import audio1 from './myOrdinaryLife.mp3';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Homepage from './components/homepage/Homepage';
-import Pagetwo from './components/pagetwo/Pagetwo';
-import Navbar from './components/Navbar';
+import Homepage from './components/homepage/Homepage ';
+import Pagetwo from './components/pagetwo/Pagetwo ';
 
 function App() {
   return (
