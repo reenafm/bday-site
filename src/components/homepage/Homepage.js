747a6d7a8ab171c3a639a70000c6bc8153e838ca
reenafm <reenafied@gmail.com>
2025-08-19 02:12:50 -0700

    fith commit

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,37 +1,24 @@
 import logo from '../../logo.svg';
 import '../../App.css';
 import audio1 from '../../myOrdinaryLife.mp3';
-import Wave from 'react-wavify'
 import { Link } from 'react-router-dom';
 
 function Homepage() {
   return (
     <div className="Homepage">
-      <h1>HAPPY 17TH BIRTHDAY MARIAN!!!!</h1>
       
-      <div className='name'>
+      <div className='quack'>
        <Link to="pagetwo">
-        <img src="https://i.imgur.com/N9J230M.jpeg"  ></img>
+        <center><img src="https://i.imgur.com/N9J230M.jpeg"  ></img></center>
         </Link>
       </div>
 
       <audio autoplay loop audio1 type="audio/mpeg">
            <source src={audio1} type="audio/mpeg"/> 
       </audio>
-      
-      <i class="las la-volume-up"></i>
-
-      <p>Helo</p>
-      <Wave fill='#ff6c6cff'
-        paused={false}
-        style={{ display: 'flex' , position: 'absolute' , bottom: '0px'}}
-        options={{
-          height: 20,
-          amplitude: 30,
-          speed: 0.15,
-          points: 3
-        }}
-  />
+      <div>
+        <i class="las la-volume-up"></i>
+      </div>
     </div>
   );
 }
--- src/components/pagetwo/Pagetwo.js
+++ src/components/pagetwo/Pagetwo.js
@@ -2,11 +2,12 @@ import logo from '../../logo.svg';
 import '../../App.css';
 import audio1 from '../../myOrdinaryLife.mp3';
 import { Link } from 'react-router-dom';
+import Wave from 'react-wavify'
 
 function Pagetwo() {
   return (
     <div className="Pagetwo">
-      <h1>I</h1>
+          <h1>HAPPY 17TH BIRTHDAY MARIAN!!!!</h1>
       
       <div className='name'>
         <Link to="/bday-site">
@@ -16,7 +17,16 @@ function Pagetwo() {
            <source src={audio1} type="audio/mpeg"/> 
         </audio>
       </div>
-      
+      <Wave fill='#ff6c6cff'
+        paused={false}
+        style={{ display: 'flex' , position: 'absolute' , bottom: '0px'}}
+        options={{
+          height: 20,
+          amplitude: 30,
+          speed: 0.15,
+          points: 3
+        }}
+  />
       <p>Helo</p>
     </div>
   );
